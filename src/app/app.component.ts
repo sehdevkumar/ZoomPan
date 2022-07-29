b9ab50b63014529b03866a0c0c7462d36d4d6a3f
sehdev <sehdev@atai.ai>
2022-07-29 17:50:11 +0530

    frist commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'drag-drop';
+  dragList: Array<string> = [
+    'Drag1',
+    'Drag2',
+    'Drag3',
+    'Drag4',
+    'Drag5',
+    'Drag6',
+  ];
+
+  dropList: Array<string> = [];
+
+  // tslint:disable-next-line:typedef
+  onDrop(event: CdkDragDrop<Array<string>>) {
+    // moveItemInArray(
+    //   event.container.data,
+    //   event.previousIndex,
+    //   event.currentIndex
+    // );
+    console.log(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
