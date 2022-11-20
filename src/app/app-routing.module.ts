ff6d4a8f7eb68c51b0b881762005d7b4dee61c56
srikarreddy.vundi <srikarreddy.vundi@invecas.com>
2022-11-20 19:45:53 +0530

    diff the changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'ZoomPan/drag-drop',
+    component:AppComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
